32dd1a14a33e173b01855e8e2a30cdeab1e07c93
Allan Araujo Lima <62760029+Allan-Araujo-Lima@users.noreply.github.com>
2024-06-26 12:28:05 -0300

    fech: ajuste no java script das horas extras para conseguir armazenr o valor do salário

--- static/js/calculadoras/hora-extra.js
+++ static/js/calculadoras/hora-extra.js
@@ -1,3 +1,11 @@
+function calculate(data) {
+    console.log(data)
+}
+
 document.querySelector(".container-hora-extra").addEventListener("submit", (e) => {
-    e.preventDefaut();
+    e.preventDefault();
+    const data = Object.fromEntries(new FormData(e.target).entries());
+    let salario = document.getElementById("salario");
+    console.log(salario);
+    console.log(data);
 })
